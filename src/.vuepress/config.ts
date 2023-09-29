b43a5093cd7e7cddf9cac67d34373492097b4309
Tanya7z <2131595226@qq.com>
2023-09-29 15:20:50 +0800

    deploy

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,18 +8,12 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "东方犬明湖 文档站",
   description: "DogeLake Docs",
-  port: 29039,
 
   theme,
   plugins: [
     searchProPlugin({
       // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-     
+      indexContent: true
     }),
   ]
-
-  // Enable it with pwa
-  // shouldPrefetch: false,
 });
